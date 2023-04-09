79a4c26a67748e0949bfa62cc937254809751cba
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-04-09 16:53:14 -0300

    .env

--- cfg.php
+++ cfg.php
@@ -1,4 +1,7 @@
 <?php
+if(!file_exists('.env')){
+	die('cd "'.__DIR__.'" && cp .env.example .env && nano .env');
+}
 require 'vendor/autoload.php';
 // https://www.php.net/manual/en/timezones.php
 date_default_timezone_set($_ENV['TIMEZONE']);
